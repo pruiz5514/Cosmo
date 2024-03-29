bf7e172a748582bc91fe2f31f2e1388f4b03abe7
pruiz5514 <pablo_ruiza@hotmail.com>
2024-03-28 19:28:37 -0700

    Se hace el home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'
+import { CursosComponent } from './pages/cursos/cursos.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path:'cursos',
+        component: CursosComponent
+    }
+];

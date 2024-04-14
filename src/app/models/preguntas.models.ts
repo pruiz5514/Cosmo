cd25e1dfbb70ce63c4f6e2c88835e775763ed040
pruiz5514 <pablo_ruiza@hotmail.com>
2024-04-14 12:25:51 -0700

    Se agregan opcion de imagenes en la respuesta multiple

--- src/app/models/preguntas.models.ts
+++ src/app/models/preguntas.models.ts
@@ -6,9 +6,13 @@ export interface Preguntas{
     enunciado: string,
     imgEnunciado: string |null,
     pregunta: string,
-    a: string | number,
-    b: string | number,
-    c: string | number,
-    d: string | number,
+    a: string | number | null,
+    imgA: string |null,
+    b: string | number | null,
+    imgB: string |null,
+    c: string | number | null,
+    imgC: string |null,
+    d: string | number | null,
+    imgD: string |null,
     completed: boolean
-}
+}3
